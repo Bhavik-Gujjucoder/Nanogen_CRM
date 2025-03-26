dcca4f4170fbf8f2d1dbb2d3b5fba6562bc70512
Bhavik-Gujjucoder <155234499+Bhavik-Gujjucoder@users.noreply.github.com>
2025-03-26 17:18:18 +0530

    softdelete add in grade management master

--- app/Models/GradeManagement.php
+++ app/Models/GradeManagement.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GradeManagement extends Model
 {
+    use SoftDeletes;
     protected $table = 'grade_management';
     protected $guarded = [];
 
