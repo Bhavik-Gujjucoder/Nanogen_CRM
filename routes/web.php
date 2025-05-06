cb5bcaf04d7dc3bf39da2a032e47a64e11f2dee4
Bhavik-Gujjucoder <bhavikg.gc@gmail.com>
2025-05-06 17:27:04 +0530

    home page update and o-form upload

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/test-mail', function () {
 });
 Route::get('/clear', function () {
     Artisan::call('cache:clear');
-    // Artisan::call('storage:link');
+    Artisan::call('storage:link');
     Artisan::call('config:cache');
     Artisan::call('optimize:clear');
     return 'Cache Clear Succesfully...';
