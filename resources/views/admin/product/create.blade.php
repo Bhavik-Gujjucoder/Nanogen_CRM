664783c6a1d3543987959c74dcc0b49652a74e3f
Bhavik-Gujjucoder <bhavikg.gc@gmail.com>
2025-06-06 18:25:48 +0530

    design changes add

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -105,13 +105,13 @@
                                 <div class="field-group field-group-new">
 
                                     {{-- <div class="field-group"> --}}
-                                    <div>
+                                    <div class="mb-3">
                                         <label class="col-form-label">Dealer Price <span
                                                 class="text-danger">*</span></label>
                                         <input type="number" name="dealer_price[]" placeholder="Dealer Price"
                                             class="form-control">
                                     </div>
-                                    <div>
+                                    <div class="mb-3">
                                         <label class="col-form-label">Distributor Price <span
                                                 class="text-danger">*</span></label>
                                         <input type="number" name="distributor_price[]" placeholder="Distributor Price"
@@ -127,7 +127,7 @@
                                                 @endforeach
                                         </select>
                                     </div>
-                                    <div>
+                                    <div class="mb-3">
                                         <label class="col-form-label">Variation Value <span
                                                 class="text-danger">*</span></label>
                                         <select class="select addfileddrop" name="variation_option_id[]">
