5e457ef06c5ac252143ebcc3e7ca5a3fc3ed7969
Bhavik-Gujjucoder <bhavikg.gc@gmail.com>
2025-06-06 14:40:28 +0530

    design changes add

--- resources/views/admin/complain/index.blade.php
+++ resources/views/admin/complain/index.blade.php
@@ -5,81 +5,89 @@
 @endsection
 <div class="card">
     <div class="card-header">
-    <!-- Search -->
-    <div class="row align-items-center">
-        <div class="col-sm-4">
-            <div class="icon-form mb-3 mb-sm-0">
-            <span class="form-icon"><i class="ti ti-search"></i></span>
-            <input type="text" class="form-control" id="customSearch" placeholder="Search User">
-            </div>
-        </div>
-        <div class="col-sm-8">
-            <div
-            class="d-flex align-items-center flex-wrap row-gap-2 column-gap-1 justify-content-sm-end gc-complain-menu">
-            <div class="dropdown me-2">
-                <div class="dropdown-menu  dropdown-menu-end">
-                    <ul>
-                        <li>
-                        <a href="javascript:void(0);" class="dropdown-item"><i
-                            class="ti ti-file-type-pdf text-danger me-1"></i>Export
-                        as PDF</a>
-                        </li>
-                        <li>
-                        <a href="javascript:void(0);" class="dropdown-item"><i
-                            class="ti ti-file-type-xls text-green me-1"></i>Export
-                        as Excel </a>
-                        </li>
-                    </ul>
+        <!-- Search -->
+        <div class="row align-items-center">
+            <div class="col-sm-4">
+                <div class="icon-form mb-3 mb-sm-0">
+                    <span class="form-icon"><i class="ti ti-search"></i></span>
+                    <input type="text" class="form-control" id="customSearch" placeholder="Search User">
                 </div>
             </div>
-                <a href="{{ route('complain.create')}}" class="btn btn-primary"><i
-                class="ti ti-square-rounded-plus me-2"></i>Complain</a>
+            <div class="col-sm-8">
+                <div
+                    class="d-flex align-items-center flex-wrap row-gap-2 column-gap-1 justify-content-sm-end gc-complain-menu">
+                    <div class="dropdown me-2">
+                        <div class="dropdown-menu  dropdown-menu-end">
+                            <ul>
+                                <li>
+                                    <a href="javascript:void(0);" class="dropdown-item"><i
+                                            class="ti ti-file-type-pdf text-danger me-1"></i>Export
+                                        as PDF</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);" class="dropdown-item"><i
+                                            class="ti ti-file-type-xls text-green me-1"></i>Export
+                                        as Excel </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="{{ route('complain.create') }}" class="btn btn-primary"><i
+                            class="ti ti-square-rounded-plus me-2"></i>Complain</a>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- /Search -->
+        <!-- /Search -->
     </div>
     <div class="card-body">
-    <!-- /Filter -->
-    <!-- dealers Users List -->
-    <div class="table-responsive custom-table">
-        <table class="table dataTable no-footer" id="complain">
-            <button class="btn btn-primary" id="bulk_delete_button" style="display: none;">Delete Selected</button>
-            <thead class="thead-light">
-            <tr>
-                <th hidden>ID</th>
-                <th class="no-sort" scope="col">
-                    <label class="checkboxs">
-                        <input type="checkbox" id="select-all" class="complain_checkbox"><span class="checkmarks"></span>
-                    </label>
-                </th>
-                <th  scope="col">Dealer/Distributor</th>
-                <th  scope="col">Date</th>
-                <th  scope="col">Products </th>
-                <th  scope="col">Status </th>
-                <th  scope="col">Remarks</th>
-                <th  scope="col">Action</th>
-            </tr>
-            </thead>
-        </table>
+        <!-- /Filter -->
+        <!-- dealers Users List -->
+        <div class="table-responsive custom-table">
+            <table class="table dataTable no-footer" id="complain">
+                <button class="btn btn-primary" id="bulk_delete_button" style="display: none;">Delete Selected</button>
+                <thead class="thead-light">
+                    <tr>
+                        <th hidden>ID</th>
+                        <th class="no-sort" scope="col">
+                            <label class="checkboxs">
+                                <input type="checkbox" id="select-all" class="complain_checkbox"><span
+                                    class="checkmarks"></span>
+                            </label>
+                        </th>
+                        <th class="no-sort" scope="col">Sr no</th>
+                        <th scope="col">Dealer/Distributor</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Products </th>
+                        <th scope="col">Status </th>
+                        <th scope="col">Remarks</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+
     </div>
-    
-    </div>
-    </div>
+</div>
 @endsection
 @section('script')
 <script>
-     var complain_table = $('#complain').DataTable({
+    var complain_table = $('#complain').DataTable({
         "pageLength": 10,
         deferRender: true, // Prevents unnecessary DOM rendering
         processing: true,
         serverSide: true,
         responsive: true,
         dom: 'lrtip',
-        order: [[0, 'desc']],  
+        order: [
+            [0, 'desc']
+        ],
         ajax: "{{ route('complain.index') }}",
-        columns: [
-            { data: 'id', name: 'id', visible: false, searchable: false },
+        columns: [{
+                data: 'id',
+                name: 'id',
+                visible: false,
+                searchable: false
+            },
             {
                 data: 'checkbox',
                 name: 'checkbox',
@@ -87,10 +95,17 @@
                 searchable: false
             },
             {
+                data: 'DT_RowIndex',
+                name: 'DT_RowIndex',
+                orderable: false,
+                searchable: false
+            }, // Auto-increment number
+            {
                 data: 'name',
                 name: 'dd_id',
                 searchable: true
             },
+
             {
                 data: 'date',
                 name: 'date',
@@ -110,7 +125,7 @@
                 data: 'remark',
                 name: 'remark',
                 searchable: true
-            },           
+            },
             {
                 data: 'action',
                 name: 'action',
@@ -118,6 +133,62 @@
                 searchable: false
             },
         ],
+        columnDefs: [{
+                targets: 0, // ID (hidden)
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'ID');
+                }
+            },
+            {
+                targets: 1, // Checkbox
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'Select');
+                }
+            },
+            {
+                targets: 2, // Sr no
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'Sr. No.');
+                }
+            },
+            {
+                targets: 3, // Dealer/Distributor
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'Dealer/Distributor');
+                }
+            },
+            {
+                targets: 4, // Date
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'Date');
+                }
+            },
+            {
+                targets: 5, // Products
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'Products');
+                }
+            },
+            {
+                targets: 6, // Status
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'Status');
+                }
+            },
+            {
+                targets: 7, // Remarks
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'Remarks');
+                }
+            },
+            {
+                targets: 8, // Action
+                createdCell: function(td) {
+                    $(td).attr('data-label', 'Action');
+                }
+            }
+        ]
+
 
     });
 
@@ -134,7 +205,7 @@
 
 
     $(document).on('click', '.deletecomplain', function(event) {
-    event.preventDefault();
+        event.preventDefault();
         let complainId = $(this).data('id'); // Get the user ID
         let form = $('#delete-form-' + complainId); // Select the correct form
 
@@ -166,19 +237,19 @@
     }
 
 
-    $(document).on('change', '.complain_checkbox', function () {
+    $(document).on('change', '.complain_checkbox', function() {
         let count = $('.complain_checkbox:checked').length; // Count checked checkboxes
         $('#checked-count').text(count); // Display count in an element
-        if(count > 0){
+        if (count > 0) {
             $('#bulk_delete_button').show();
-        }else{
+        } else {
             $('#bulk_delete_button').hide();
         }
     });
 
     // Handle Bulk Delete button click
     $('#bulk_delete_button').click(function() {
-       confirmDeletion(function() {
+        confirmDeletion(function() {
             var selectedIds = $('.complain_checkbox:checked').map(function() {
                 return $(this).data('id');
             }).get();
@@ -188,7 +259,7 @@
                     url: "{{ route('complain.bulkDelete') }}",
                     method: 'POST',
                     data: {
-                        ids: selectedIds, 
+                        ids: selectedIds,
                         _token: '{{ csrf_token() }}'
                     },
                     success: function(response) {
